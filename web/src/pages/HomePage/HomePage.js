f0989474e44d9d1eaeeef5136d804bcbaf33f3ec
patrick <blockchainbuddha@gmail.com>
2022-02-08 01:54:21 -0800

    Add two copy areas

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -47,9 +47,10 @@ const HomePage = () => {
     await dao({ variables: data })
   }
 
-  const snippet = `<a href="${data?.dao?.daoHausUrl}">
+  const buttonSnippet = `<a href="${data?.dao?.daoHausUrl}">
   <img src="${process.env.APP_DOMAIN}/api/canvas?chainId=${values.chainId}&address=${values.contractAddr}" alt="${data?.dao?.name}" />
 </a>`
+  const imageSnippet = `${process.env.APP_DOMAIN}/api/canvas?chainId=${values.chainId}&address=${values.contractAddr}`
 
   return (
     <>
@@ -169,25 +170,48 @@ const HomePage = () => {
         </div>
         <div>
           {data && !loading && (
-            <div className="p-4 bg-black rounded-lg">
-              <textarea
-                rows="5"
-                className="bg-gray-900 text-white text-sm w-full p-4 rounded pointer-events-none"
-                id="snippet"
-                value={snippet}
-                readOnly
-              >
-                {snippet}
-              </textarea>
-              <button
-                className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
-                id="copyButton"
-                data-clipboard-action="copy"
-                data-clipboard-target="#snippet"
-              >
-                Copy
-              </button>
-            </div>
+            <>
+              <div className="p-4 bg-black rounded-lg">
+                <p>Image</p>
+                <textarea
+                  rows="2"
+                  className="bg-gray-900 text-white text-sm w-full p-4 rounded pointer-events-none"
+                  id="imageSnippet"
+                  value={imageSnippet}
+                  readOnly
+                >
+                  {imageSnippet}
+                </textarea>
+                <button
+                  className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+                  id="copyButton"
+                  data-clipboard-action="copy"
+                  data-clipboard-target="#imageSnippet"
+                >
+                  Copy
+                </button>
+              </div>
+              <div className="p-4 bg-black rounded-lg">
+                <p>Button</p>
+                <textarea
+                  rows="5"
+                  className="bg-gray-900 text-white text-sm w-full p-4 rounded pointer-events-none"
+                  id="buttonSnippet"
+                  value={buttonSnippet}
+                  readOnly
+                >
+                  {buttonSnippet}
+                </textarea>
+                <button
+                  className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+                  id="copyButton"
+                  data-clipboard-action="copy"
+                  data-clipboard-target="#buttonSnippet"
+                >
+                  Copy
+                </button>
+              </div>
+            </>
           )}
         </div>
       </div>
